dbde6774de3b35045906fecbf9a9647ef613ff07
Thais Lira <thais__lira@hotmail.com>
2026-02-24 12:49:57 +0000

    pag login

--- app/src/main/java/com/example/gamecodex_app/MainActivity.kt
+++ app/src/main/java/com/example/gamecodex_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gamecodex_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gamecodex_app.databinding.ActivityMainBinding
 
@@ -16,10 +18,35 @@ class MainActivity : AppCompatActivity() {
 
         val arrayLoginsValidos = ArrayList<Login>()
         arrayLoginsValidos.add(Login("admin", "123456", "Administrador", "1990"))
-        arrayLoginsValidos.add(Login("user1", "pass1", "João Silva", "2005"))
+        arrayLoginsValidos.add(Login("thais", "pass1", "Thais Lira", "1993"))
+        arrayLoginsValidos.add(Login("maria", "pass2", "Maria Madalena", "2010"))
+        arrayLoginsValidos.add(Login("joana", "pass3", "Joana Dark", "2005"))
+        arrayLoginsValidos.add(Login("leo", "pass4", "Leonardo Aguiar", "1999"))
 
+        binding.buttonLogin.setOnClickListener {
+            val userIn = binding.editUsername.text.toString()
+            val passIn = binding.editPassword.text.toString()
 
+            var validUser: Login? = null
 
+            for (validUser in arrayLoginsValidos) {
+                if (validUser.username == userIn && validUser.password == passIn) {
+                    break
+                }
+            }
+
+            if (validUser != null) {
+                val intent = Intent(this, WishlistActivity::class.java)
+
+                intent.putExtra("NOME_USER", validUser.name)
+                intent.putExtra("ANO_NASC", validUser.birthday.toInt())
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login not valid, try again!", Toast.LENGTH_SHORT).show()
+                binding.editUsername.text.clear()
+                binding.editPassword.text.clear()
+            }
+        }
     }
 }
-
--- app/src/main/java/com/example/gamecodex_app/WishlistActivity.kt
+++ app/src/main/java/com/example/gamecodex_app/WishlistActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gamecodex_app
 
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gamecodex_app.databinding.ActivityWishlistBinding
 
@@ -14,6 +16,30 @@ class WishlistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-    }
+        val arrayListCatalogue = ArrayList<Game>()
 
+        arrayListCatalogue.add(Game("Pokemon Scarlet", "RPG", 59.99, 2022, 12, 0.5))
+        arrayListCatalogue.add(Game("Elden Ring", "Ação/RPG", 69.90, 2022, 16, 0.6))
+        arrayListCatalogue.add(Game("Super Mario Odyssey", "Plataforma", 45.00, 2017, 3, 0.4))
+        arrayListCatalogue.add(Game("God of War Ragnarok", "Ação", 79.99, 2022, 18, 0.8))
+        arrayListCatalogue.add(Game("FIFA 23", "Desporto", 69.99, 2022, 3, 0.3))
+        arrayListCatalogue.add(Game("The Last of Us Part II", "Aventura", 39.90, 2020, 18, 0.7))
+        arrayListCatalogue.add(Game("Stray", "Aventura", 29.99, 2022, 12, 0.2))
+        arrayListCatalogue.add(Game("Minecraft", "Sandbox", 25.00, 2011, 7, 0.1))
+        arrayListCatalogue.add(Game("Cyberpunk 2077", "RPG", 49.99, 2020, 18, 0.6))
+        arrayListCatalogue.add(Game("Animal Crossing", "Simulação", 50.00, 2020, 3, 0.4))
+        arrayListCatalogue.add(Game("Hades", "Indie/Roguelike", 24.99, 2020, 12, 0.2))
+        arrayListCatalogue.add(Game("Call of Duty: MWII", "FPS", 75.00, 2022, 18, 0.9))
+        arrayListCatalogue.add(Game("The Witcher 3", "RPG", 30.00, 2015, 18, 0.7))
+        arrayListCatalogue.add(Game("Tetris Effect", "Puzzle", 19.99, 2018, 3, 0.1))
+        arrayListCatalogue.add(Game("Zelda: Breath of the Wild", "Aventura", 60.00, 2017, 12, 0.5))
+
+
+        var arrayAdapterCatalogue =
+            ArrayAdapter(this, R.layout.simple_list_item_1, arrayListCatalogue)
+
+        binding.listCatalogue.adapter = arrayAdapterCatalogue
+
+
+    }
 }
